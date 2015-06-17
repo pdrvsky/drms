132f094d069d8a58165759be95dff0678aa92374
pdrvsky <prtestpl@gmail.com>
2015-06-17 17:14:50 +0200

    Initial commit

---
+++ drms/Classes/MediaFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace drms.Classes
+{
+    [Serializable]
+    public struct MediaFile
+    {
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Title { get; set; }
+        public string Path { get; set; }
+        public uint Year { get; set; }
+        public uint Track { get; set; }
+        public byte[] Artwork { get; set; }
+    }
+}
